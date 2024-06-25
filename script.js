9b1a96e4738436c55309a54164e3927b57cb7236
rjba <117646976+YorNitsuga@users.noreply.github.com>
2024-06-25 09:34:46 +0800

    Finish the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let compChoice = Math.floor(Math.random() * 3) + 1;
+    console.log(compChoice);
+
+    if (compChoice === 1) {
+        return "rock";
+    } else if (compChoice === 2) {
+        return "paper";
+    } else if (compChoice === 3) {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice())
