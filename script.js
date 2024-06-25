c7d7e0b4ad24a295d8a388b8d1c6d04648bdf230
rjba <117646976+YorNitsuga@users.noreply.github.com>
2024-06-25 10:51:11 +0800

    Finish the logic to play the entire game

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-let humanScore = 0;
-let computerScore = 0;
-
-let compChoice = Math.floor(Math.random() * 3) + 1;
-
 function getComputerChoice() {
-    // console.log(compChoice);
+    let compChoice = Math.floor(Math.random() * 3) + 1;
     if (compChoice === 1) {
         return "rock";
     } else if (compChoice === 2) {
@@ -14,13 +9,8 @@ function getComputerChoice() {
     }
 }
 
-// Just to check the value of compChoice for testing
-console.log(getComputerChoice())
-
-let humanChoice = prompt("Choose between: rock, paper, scissors").toLowerCase();
-
 function getHumanChoice() {
-
+    let humanChoice = prompt("Choose between: rock, paper, scissors").toLowerCase();
     if (humanChoice === "rock") {
         return "rock";
     } else if (humanChoice === "paper") {
@@ -30,38 +20,46 @@ function getHumanChoice() {
     }
 }
 
-// Just to check the value of humanChoice for testing
-console.log(getHumanChoice());
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-function playRound(humanChoice, compChoice) {
-    if (humanChoice === "rock" && compChoice === "rock") {
-        console.log(`It's a tie! You both chose rock!`);
-    } else if (humanChoice === "rock" && compChoice === "paper") {
-        computerScore++;
-        console.log(`You lost! ${humanChoice} gets beaten by ${compChoice}`);
-    } else if (humanChoice === "rock" && compChoice === "scissors") {
-        humanScore++;
-        console.log(`You won! ${humanChoice} beats ${compChoice}`);
-    } else if (humanChoice === "paper" && compChoice === "paper") {
-        console.log("It's a tie! You both chose paper!");
-    } else if (humanChoice === "paper" && compChoice === "scissors") {
-        computerScore++;
-        console.log(`You lost! ${humanChoice} gets beaten by ${compChoice}`);
-    } else if (humanChoice === "paper" && compChoice === "rock") {
-        humanScore++;
-        console.log(`You won! ${humanChoice} beats ${compChoice}`);
-    } else if (humanChoice === "scissors" && compChoice === "scissors") {
-        console.log("It's a tie! You both chose scissors!");
-    } else if (humanChoice === "scissors" && compChoice === "rock") {
-        computerScore++;
-        console.log(`You lost! ${humanChoice} gets beaten by ${compChoice}`);
-    } else if (humanChoice === "scissors" && compChoice === "paper") {
-        humanScore++;
-        console.log(`You won! ${humanChoice} beats ${compChoice}`);
+    function playRound(humanChoice, compChoice) {
+        if (humanChoice === "rock" && compChoice === "rock") {
+            console.log(`It's a tie! You both chose rock!`);
+        } else if (humanChoice === "rock" && compChoice === "paper") {
+            computerScore++;
+            console.log(`You lost! ${humanChoice} gets beaten by ${compChoice}`);
+        } else if (humanChoice === "rock" && compChoice === "scissors") {
+            humanScore++;
+            console.log(`You won! ${humanChoice} beats ${compChoice}`);
+        } else if (humanChoice === "paper" && compChoice === "paper") {
+            console.log("It's a tie! You both chose paper!");
+        } else if (humanChoice === "paper" && compChoice === "scissors") {
+            computerScore++;
+            console.log(`You lost! ${humanChoice} gets beaten by ${compChoice}`);
+        } else if (humanChoice === "paper" && compChoice === "rock") {
+            humanScore++;
+            console.log(`You won! ${humanChoice} beats ${compChoice}`);
+        } else if (humanChoice === "scissors" && compChoice === "scissors") {
+            console.log("It's a tie! You both chose scissors!");
+        } else if (humanChoice === "scissors" && compChoice === "rock") {
+            computerScore++;
+            console.log(`You lost! ${humanChoice} gets beaten by ${compChoice}`);
+        } else if (humanChoice === "scissors" && compChoice === "paper") {
+            humanScore++;
+            console.log(`You won! ${humanChoice} beats ${compChoice}`);
+        }
     }
+
+    playRound(getHumanChoice(),getComputerChoice());
+    playRound(getHumanChoice(),getComputerChoice());
+    playRound(getHumanChoice(),getComputerChoice());
+    playRound(getHumanChoice(),getComputerChoice());
+    playRound(getHumanChoice(),getComputerChoice());
+    
+    console.log(`Your score: ${humanScore}`);
+    console.log(`Computer score: ${computerScore}`);
 }
 
-playRound(getHumanChoice(), getComputerChoice());
-
-console.log(`Your score: ${humanScore}`);
-console.log(`Computer score: ${computerScore}`);
+playGame();
